7dfe53d0cfbec96514d0b1f80b5adf4c8b3a3bf2
leonardmorina <leonardmorina1@gmail.com>
2023-02-23 02:37:09 +0100

    supressing the non nullables

--- src/Erp.Api/Program.cs
+++ src/Erp.Api/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 {
     options.Conventions.Add(new RouteTokenTransformerConvention(new SlugifyParameterTransformer()));
+    options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
